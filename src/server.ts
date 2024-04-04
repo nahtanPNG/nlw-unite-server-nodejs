cff7887a8e1a531bb4e8587d45bff334fde1d1af
Nathan Ferreira <nathan.ferreiira13@gmail.com>
2024-04-04 15:28:33 -0300

    Aula 01: Concluida | Inserindo um registro

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
 
@@ -12,11 +18,30 @@ const app = fastify();
 // Driver nativo(Construído a mão) / Query Builders / ORMs
 
 //Object Relational Mapping (Hibernate / Docktrine / ActiveRecord) -> Ferramenta que automatiza varios processos do banco de dados ao mesmo tempo
+// json -> JavaScript Object Notation -> Liguagem de transferência de dados
 
-app.get('/', () => {
-    return 'Hello World!'
+app.post("/events", async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(request.body) //Validando se o request.body segue a estrutura acima
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  })
+
+  return reply.status(201).send({ eventId: event.id })
 })
 
-app.listen({ port: 3333}).then(() => { //.then() -> Acontece quando der certo a promisse
-    console.log("HTTP server running on http://localhost:3333")
+app.listen({ port: 3333 }).then(() => {
+  //.then() -> Acontece quando der certo a promisse
+  console.log("HTTP server running on http://localhost:3333");
 })
