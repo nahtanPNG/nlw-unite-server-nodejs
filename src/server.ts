d1c338b6b6d791625a3756e76dff3220f1abc3ad
Nathan Ferreira <nathan.ferreiira13@gmail.com>
2024-04-22 12:40:32 -0300

    feat: :sparkles: Create GET routes for the event and attendee

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -10,6 +12,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => { //.then() -> Acontece quando der certo a promisse
   console.log("HTTP server running on http://localhost:3333");
